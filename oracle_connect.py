c759582fe206ad4ec59fd037ad93f51839403951
KBlackwell <43302501+KRBlackwell@users.noreply.github.com>
2024-09-10 09:58:30 -0400

    Update oracle_connect.py: add query method and a few queries

--- oracle_connect.py
+++ oracle_connect.py
@@ -16,8 +16,21 @@ instant_client_dir = None
 
 oracledb.init_oracle_client(lib_dir=instant_client_dir)
 print("oracledb client version:", oracledb.clientversion())
+
 if PASSWORD is None:
   PASSWORD = getpass.getpass("Enter password for %s:" % user)
+
+def do_query(query):
+  cursor.execute(query)
+  rows = cursor.fetchall()
+  print()
+  print()
+  print("executed sql query:", query)
+  try:
+    for row in rows:
+      print(row)
+  except Exception as e:
+    print(e)
 
 try:
   connection = oracledb.connect(
@@ -31,19 +44,21 @@ except Exception as e:
 
 cursor = connection.cursor()
 print("connection: got cursor")
-query = """SELECT table_name
-           FROM (SELECT table_name
-                 FROM all_tables
-                 ORDER BY table_name)
-          WHERE ROWNUM <= 5"""
-cursor.execute(query)
-rows = cursor.fetchall()
-print("executed query")
-try:
-  for row in rows:
-    print(row)
-except Exception as e: 
-  print(e)
-finally:
-  cursor.close()
-  connection.close()
+
+#this is a good query
+#query = """SELECT table_name
+#           FROM (SELECT table_name
+#                 FROM all_tables
+#                 ORDER BY table_name)
+#          WHERE ROWNUM <= 5"""
+
+query1 = """SELECT * FROM ALL_TAB_COLUMNS WHERE OWNER='EXAMPLE'"""
+query2 = """SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'PERSON_DEMO'"""
+query3 = """SELECT * FROM all_tables where table_name = 'PERSON_DEMO'"""
+
+do_query(query1)
+do_query(query2)
+do_query(query3)
+
+cursor.close()
+connection.close()
